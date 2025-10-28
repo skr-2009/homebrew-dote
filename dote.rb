e6350e4871233cdda7dee571fd827541b664754c
skr-2009 <kmc2532@kamiyama.ac.jp>
2025-10-28 11:25:15 +0900

    version “1.0.0”

--- dote.rb
+++ dote.rb
@@ -3,7 +3,7 @@ class Dote < Formula
   homepage "https://github.com/skr-2009/homebrew-dote"
   url "https://raw.githubusercontent.com/skr-2009/homebrew-dote/main/dote.c"
   sha256 "947696b428732d93971c981a57c38fbbeea616c9bf0788e4d0f621cb441f0e31"
-  version “1.0.0”
+  version "1.0.0"
 
   def install
     system ENV.cc, "dote.c", "-o", "dote"
