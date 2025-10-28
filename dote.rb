1ff6425d4d4f7980b641154395761e652fb0f67d
skr-2009 <kmc2532@kamiyama.ac.jp>
2025-10-28 11:59:16 +0900

    Update dote.rb

--- dote.rb
+++ dote.rb
@@ -2,7 +2,7 @@ class Dote < Formula
   desc "dot e command"
   homepage "https://github.com/skr-2009/homebrew-dote"
   url "https://raw.githubusercontent.com/skr-2009/homebrew-dote/main/dote.c"
-  sha256 "947696b428732d93971c981a57c38fbbeea616c9bf0788e4d0f621cb441f0e31"
+  sha256 "7bd13199b5ef8605a9599224376da7825f2f6e072e8ea3451aaf03573eadec10"
   version "1.0.0"
 
   def install
